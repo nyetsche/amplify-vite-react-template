c9c18450dff566bae2ce74048769e0597e111f7c
Matthew Lesko <matthew.w.lesko@nasa.gov>
2024-07-04 13:24:55 -0400

    save attempt at creating s3 bucket

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,18 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { S3MapStack } from './custom/maps/resource';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+new S3MapStack( backend.createStack('mlesko-QUUX-S3MapStack'), 'amplify-amplifyvitereacttemplate-mlesko-sandbox-2045a380c9/mlesko-QUUX-S3MapStack/Default')
+
+/*
+const bucketStack = backend.getStack('BucketStack');
+const bucket = new s3.Bucket(bucketStack, 'CCMC-mlesko-QUUX-amplify-mint-perch', {
+  blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL
+});
+*/
